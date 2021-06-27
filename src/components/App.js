6ec0a04466211fba283d0ed790d0e239e628ffe8
Thomas Glenn <thomasgrantglenn@mac.com>
2021-06-26 23:28:28 -0600

    add App.js, Header, and book files to components folder.

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from './Header'
+import { Router, Route, Switch } from 'react-router-dom'
+
+const App = () => {
+  return (
+  <p>Hello</p>,
+
+  <Header />
+  )
+}
+
+export default App
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Header = () => {
+
+  return <p>Welcome to Class</p>
+
+}
+
+export default Header
