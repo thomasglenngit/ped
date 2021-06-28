d855d1188625cdfa566cea1e5d971ffff3052aa6
Thomas Glenn <thomasgrantglenn@mac.com>
2021-06-28 10:42:17 -0600

    add BrowserRouter to App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,40 @@
 import React from 'react'
 import Header from './Header'
-import { Router, Route, Switch } from 'react-router-dom'
-import history from '../history'
+import { BrowserRouter, Switch, Route, } from 'react-router-dom'
+import Profile from './Profile'
+import Brown from './Brown'
+import Coyle from './Coyle'
+import Ristad from './Ristad'
+import Smith from './Smith'
+// import history from '../history'
 
 const App = () => {
   return (
     <p>Hello</p>,
-    <Router >
+    
+    <BrowserRouter >
       <div>
         <Header />
         <Switch>
-          <Route path="/Profile" />
-          <Route path="/Brown" />
-          <Route path="/Coyle" />
-          <Route path="/Ristad" />
-          <Route path="/Smith" />
+          <Route path="/Profile">
+            <Profile />
+          </Route>
+          <Route path="/Brown">
+            <Brown />
+          </Route>
+          <Route path="/Coyle">
+            <Coyle />
+          </Route>
+          <Route path="/Ristad">
+            <Ristad />
+          </Route>
+          <Route path="/Smith">
+            <Smith />
+          </Route>
         </Switch>
       </div>
 
-    </Router>
+    </BrowserRouter>
 
   )
 }
