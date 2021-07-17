ef72684f070c2ab37f8e4c02ab1240e3b47f085f
Thomas Glenn <thomasgrantglenn@mac.com>
2021-07-17 13:59:09 -0600

    add input fields to profile component.

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -5,13 +5,19 @@ const Profile = () => {
   return (
     <React.Fragment>
       <div className="ui container">
-        <label>First Name</label><br />
+        <label>First Name:</label><br />
         <input></input><br />
-        <label>Last Name</label><br />
+        <label>Last Name:</label><br />
         <input></input><br />
-        <label>Student id#</label><br />
+        <label>Email Address:</label><br />
         <input></input><br />
-        <label>Major</label><br />
+        <br />
+        <label>Student id#:</label><br />
+        <input></input><br />
+        <label>Major:</label><br />
+        <input></input><br />
+        <br />
+        <label>Class (eg. "Sophomore"):</label><br />
         <input></input><br />
         <br />
       </div>
